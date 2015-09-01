fe4bc6346a69f193b4101851795836aa884be5fc
davidfialho <fialho.david@gmail.com>
2015-09-01 20:03:38 +0100

    fixed bug where the file stored on dropbox was using the full path name

--- monitor.py
+++ monitor.py
@@ -3,7 +3,6 @@ import dropbox
 import os.path as path
 
 class SyncHandler(PatternMatchingEventHandler):
-    patterns = ["*.CSV"]
 
     def __init__(self, client):
         super().__init__()
@@ -12,7 +11,7 @@ class SyncHandler(PatternMatchingEventHandler):
 
     def update(self, filename):
         file = open(filename, "rb")
-        response = self.client.put_file(filename, file, overwrite=True)
+        response = self.client.put_file(path.basename(filename), file, overwrite=True)
 
     def on_modified(self, event):
         if not event.is_directory:
