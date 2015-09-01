5c2261bbbd40c66a91f1167995a89ceca38c6f7c
davidfialho <fialho.david@gmail.com>
2015-09-01 20:10:47 +0100

    implemented multiple user support

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import time
 from watchdog.observers import Observer
 import monitor
 
+keysFilename = ".keys"
+
 def authorizeDropbox(flow):
     # Have the user sign in and authorize this token
     authorizeUrl = flow.start()
@@ -14,10 +16,33 @@ def authorizeDropbox(flow):
     code = input("Enter the authorization code here: ").strip()
 
     # This will fail if the user enters an invalid authorization code
-    accessToken, userId = flow.finish(code)
+    userKey, userId = flow.finish(code)
 
-    return accessToken
+    return userKey
 
+def getUserKey(username):
+    userKey = ""    # variable to be returned with the user key
+
+    if path.exists(keysFilename):
+        # open keys file
+        keysFile = open(keysFilename, "r")
+
+        # get user key from the keys file for the given username
+        for line in keysFile.readlines():
+            index = line.find(":")
+            lineUsername = line[:index]     # the username is stored previous to the first ':' char
+            lineKey = line[index + 1:]      # the user key is stored after the first ':' char
+
+            # check if the correct username was found
+            if lineUsername == username:
+                # username found -> get access token
+                userKey = lineKey
+                # leave the loop
+                break
+
+        keysFile.close()
+
+    return userKey
 
 def main():
     appKey = "1tgi3eh3hq78u34"
@@ -26,18 +51,25 @@ def main():
     # authorize the app
     flow = dropbox.client.DropboxOAuth2FlowNoRedirect(appKey, appSecret)
 
-    if path.exists(".keys"):
-        # get access token from file
-        accessToken = open(".keys", "r").readline()
+    # get user name to identify the login
+    username = input("dropbox username: ")
 
-    else:
-        accessToken = authorizeDropbox(flow)
+    # check the database for the user key
+    userKey = getUserKey(username)
+    
+    if not userKey:
+        # user key was not found -> new user
+        # get new user authorization
+        userKey = authorizeDropbox(flow)
 
         # save access token
-        open(".keys", "w").write(accessToken)
+        keysFile = open(keysFilename, "w")
+        keysFile.write(username + ":" + userKey)
+        keysFile.flush()
+        keysFile.close()
 
-    # get a dropbox client for th access token
-    client = dropbox.client.DropboxClient(accessToken)
+    # get a dropbox client for the access token
+    client = dropbox.client.DropboxClient(userKey)
 
     # remove the first argument from the sys args
     args = sys.argv[1:]
